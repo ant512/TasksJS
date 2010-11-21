30b95438b5b9a50a47b291875d473deb29c8bbfd
ant512 <devnull@localhost>
2010-11-21 08:42:33 +0000

    Added comments.

--- project.js
+++ project.js
@@ -1,5 +1,11 @@
 var Projects = {
 
+	/**
+	 * Project object.
+	 * @param name Name of the project.
+	 * @startDate Start date of the project.
+	 * @creator The creator of the project.
+	 */
 	Project: function(name, startDate, creator) {
 		this.tasks = new Array();
 		this.name = name;
@@ -9,14 +15,25 @@ var Projects = {
 	}
 }
 
+/**
+ * Get the list of tasks.
+ * @return An array containing the project's tasks.
+ */
 Projects.Project.prototype.getTasks = function() {
 	return this.tasks;
 }
 
+/**
+ * Add a new task to the project.
+ * @param task The task to add.
+ */
 Projects.Project.prototype.addTask = function(task) {
 	this.tasks.push(task);
 }
 
+/**
+ * Recalculates the dates of all tasks in the project.
+ */
 Projects.Project.prototype.recalculateDates = function() {
 	var calc = new Tasks.DateCalculator(this.tasks);
 	calc.recalculateDates(this.startDate);
